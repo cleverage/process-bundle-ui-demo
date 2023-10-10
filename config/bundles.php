54c9ecd603c6c6528aec45fa632c79a08bf366a2
Xavier Marchegay <xmarchegay@clever-age.com>
2023-10-10 13:33:17 +0200

    add cleverage/doctrine-process-bundle + demo doctrine read

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    CleverAge\DoctrineProcessBundle\CleverAgeDoctrineProcessBundle::class => ['all' => true],
 ];
