2b2d9df55f088dd4690c94ef7980c1cd25ce9104
Tonon Grégory <gtonon@clever-age.com>
2024-04-19 10:23:38 +0200

    [Scheduler table migration] Add support for pgsql, mysql & mariadb

--- migrations/Version20240322151928.php
+++ migrations/Version20240322151928.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
+use Doctrine\DBAL\Platforms\MariaDBPlatform;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+use Doctrine\DBAL\Platforms\SqlitePlatform;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
@@ -19,9 +23,17 @@ final class Version20240322151928 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE process_schedule (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, process VARCHAR(255) NOT NULL, type VARCHAR(6) NOT NULL, expression VARCHAR(255) NOT NULL, context CLOB NOT NULL --(DC2Type:json)
-        )');
+        $platform = $this->connection->getDatabasePlatform();
+        if ($platform instanceof PostgreSQLPlatform) {
+            $this->addSql('CREATE TABLE process_schedule (id INT NOT NULL, process VARCHAR(255) NOT NULL, type VARCHAR(6) NOT NULL, expression VARCHAR(255) NOT NULL, context JSON NOT NULL, PRIMARY KEY(id))');
+        }
+        if ($platform instanceof SqlitePlatform) {
+            $this->addSql('CREATE TABLE process_schedule (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, process VARCHAR(255) NOT NULL, type VARCHAR(6) NOT NULL, expression VARCHAR(255) NOT NULL, context CLOB NOT NULL --(DC2Type:json))');
+        }
+
+        if ($platform instanceof MariaDBPlatform or $platform instanceof MySQLPlatform) {
+            $this->addSql('CREATE TABLE process_schedule (id INT NOT NULL, process VARCHAR(255) NOT NULL, type VARCHAR(6) NOT NULL, expression VARCHAR(255) NOT NULL, context JSON NOT NULL COMMENT \'(DC2Type:json)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB;');
+        }
     }
 
     public function down(Schema $schema): void
